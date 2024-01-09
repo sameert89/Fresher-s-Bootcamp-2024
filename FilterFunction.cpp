5e41591fa4167c758ae597463d6f043eb26de096
Sameer Trivedi <sameertrivedi1234@gmail.com>
2024-01-09 14:26:37 +0530

    Update FilterFunction.cpp

--- FilterFunction.cpp
+++ FilterFunction.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 
-std::vector<int> filter(const std::vector<int> &arr, std::function<bool(int)> criteria) {
+std::vector<int> filter(const std::vector<int> &nums, std::function<bool(int)> criteria) {
     std::vector<int> result;
-    for(auto const &ele: arr){
-        if(criteria(ele))
-            result.push_back(ele);
+    for(auto const &num: nums){
+        if(criteria(num))
+            result.push_back(num);
     }
     return result;
 }
 
 int main()
 {
-    std::cout<<"Hello World";
+    std::vector<int> nums = {1, 7, 55, 22, 12, 24, 16};
+    std::vector<int> evenNums = filter(nums, [](int e){return a%2 == 0});
     return 0;
 }
