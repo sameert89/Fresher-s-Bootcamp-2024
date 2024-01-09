5cb1c235d8995bddd28c1a901db70a3dfead770f
Sameer Trivedi <sameertrivedi1234@gmail.com>
2024-01-09 14:40:42 +0530

    Update FilterFunction.cpp

--- FilterFunction.cpp
+++ FilterFunction.cpp
@@ -3,8 +3,9 @@
 std::vector<int> filter(const std::vector<int> &nums, std::function<bool(int)> criteria) {
     std::vector<int> result;
     for(auto const &num: nums){
-        if(criteria(num))
+        if(criteria(num)){
             result.push_back(num);
+        }
     }
     return result;
 }
@@ -12,6 +13,6 @@ std::vector<int> filter(const std::vector<int> &nums, std::function<bool(int)> c
 int main()
 {
     std::vector<int> nums = {1, 7, 55, 22, 12, 24, 16};
-    std::vector<int> evenNums = filter(nums, [](int e){return a%2 == 0});
+    std::vector<int> evenNums = filter(nums, [](int e){ return a%2 == 0; });
     return 0;
 }
