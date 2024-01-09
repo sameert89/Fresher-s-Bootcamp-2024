6bef318b51ca902404d994d2b033fc4a66ca3f38
Sameer Trivedi <sameertrivedi1234@gmail.com>
2024-01-09 13:55:19 +0530

    Update and rename SearchFunction.cpp to FilterFunction.cpp

--- FilterFunction.cpp
+++ FilterFunction.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-std::vector<int> search(const std::vector<int> &arr, std::function<bool(int)> criteria) {
+std::vector<int> filter(const std::vector<int> &arr, std::function<bool(int)> criteria) {
     std::vector<int> result;
     for(auto const &ele: arr){
         if(criteria(ele))
