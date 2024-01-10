915cd989ffcf059fc0f663f52b308bcdf7114937
Sameer Trivedi <sameertrivedi1234@gmail.com>
2024-01-10 16:22:19 +0530

    Update Filter Function Using OOPs.cpp

--- Filter Function Using OOPs.cpp
+++ Filter Function Using OOPs.cpp
@@ -4,6 +4,62 @@
 using namespace std;
 
 /* -------- CORRECTIONS ---------*/
+
+/* -------- Aggregation --------*/
+class SearchStrategy {
+public:
+    virtual void invoke(string item) = 0;
+};
+class StartsWithStrategy : public SearchStrategy{
+private:
+    string startsWith;
+public
+    void setStartsWith(string key){
+        this->startsWith = key;
+    }
+    bool invoke(string item){
+        return item.find(this->startsWith) == 0;
+    }
+};
+
+class ConsoleDisplayController{
+private:
+    string content = "";
+public:
+    void setContent(string msg) {
+        this->content = msg;
+    }
+    void display() {
+        for(auto const &e: this->content) {
+            cout << e << ",";
+        }
+    }
+};
+class StringListFilterController{
+    private:
+        SearchStrategy* S;
+    public:
+        void setSearchStrategy(SearchStrategy* ptr){
+            this->S = ptr;
+        }
+        vector<string> filter(vector<string> strList){
+            vector<string> result;
+            for(auto const &str: strList){
+                if(SS.invoke(str)) {
+                    result.push_back(str);
+                }
+            }
+            return result;
+        }
+};
+int main(){
+    StartsWithStrategy SS;
+    SearchStrategy* ptr = &SS;
+    StringListFilterController SLF;
+    SLF.setStrategy(ptr);
+}
+
+/* -------- Composition --------*/
 class ConsoleDisplayController{
 private:
     string content = "";
