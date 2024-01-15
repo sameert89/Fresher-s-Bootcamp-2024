659980656690c22333df75e44748531da97d5d0d
Sameer Trivedi <sameertrivedi1234@gmail.com>
2024-01-15 11:11:28 +0530

    Update ObserverNotification.cpp

--- ObserverNotification.cpp
+++ ObserverNotification.cpp
@@ -27,23 +27,30 @@ class Thread{
     public:
         Thread(){
             this->state = "created";
+            this->notify(this->state);
         }
         void start(){
             this->state = "running";
+            this->notify(this->state);
         }
         void abort(){
             this->state = "aborted";
+            this->notify(this->state);
         }
         void sleep(int t){
-            this->state = "sleep";
+            this->state = "sleeping";
+            this->notify(this->state);
             sleep(t/1000);
             this->state = "running";
+            this->notify(this->state);
         }
         void wait(){
             this->state = "waiting";
+            this->notify(this->state);
         }
         void suspended(){
             this->state = "suspended";
+            this->notify(this->state);
         }
         void subscribe(IObserver* IObserverObjRef){
             this->observers.push_back(IObserverObjRef);
