8f294edec9331c60b52dada1774dac567193a64b
Sameer Trivedi <sameertrivedi1234@gmail.com>
2024-01-18 14:38:11 +0530

    Update Program.cs

--- LanguageSessions/ProductClass/Program.cs
+++ LanguageSessions/ProductClass/Program.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace ECommerce {
-    
-  class Program {
-    static void Main(string[] args) {
-      bool decision = true;
-      int choice = 1;
-      List<Product> products = new List<Product>();
+   
+ class Program {
+   static void Main(string[] args) {
+     bool decision = true;
+     int choice = 1;
+     Dictionary<int, Product> products = new Dictionary<int, Product>();
 
-      do {
-        Console.WriteLine(
-            "Please enter a choice: \n 1) Add Product, 2) Display Products, 3) Find Product, 4) Exit");
-        choice = Convert.ToInt32(Console.ReadLine());
+     do {
+       Console.WriteLine(
+           "Please enter a choice: \n 1) Add Product, 2) Display Products, 3) Find Product, 4) Delete Product, 5) Exit");
+       choice = Convert.ToInt32(Console.ReadLine());
 
-        switch (choice) {
-          case 1:
-            Console.WriteLine("Enter product ID:");
-            int prodID = Convert.ToInt32(Console.ReadLine());
+       switch (choice) {
+         case 1:
+           Console.WriteLine("Enter product ID:");
+           int prodID = Convert.ToInt32(Console.ReadLine());
 
-            if (prodID < 0)
+           if (products.ContainsKey(prodID))
+             throw new Exception("Product ID already exists");
+
+           if (prodID < 0)
               throw new Exception("Product ID cannot be negative");
 
             Console.WriteLine("Enter warranty (months):");
@@ -60,40 +63,41 @@ namespace ECommerce {
             if (price < 0)
               throw new Exception("price cannot be less than equal to 0");
 
-            products.Add(new Product(prodID, warranty, itemStock, gstPercent,
-                                     discountPercent, name, mfgDate, price));
-            break;
+           products[prodID] = new Product(prodID, warranty, itemStock, gstPercent,
+                                  discountPercent, name, mfgDate, price);
+           break;
 
-          case 2:
-            foreach (var prod in products) {
-              Console.WriteLine(prod.display());
-            }
-            break;
-          case 3:
-            Console.WriteLine("Please Enter the ProdID");
-            prodID = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(prodID);
-            bool found = false;
-            foreach(var prod in products){
-                if(prod._prodID == prodID){
-                    Console.WriteLine(prod._prodID);
-                    Console.WriteLine(prod.display());
-                    flag = true;
-                    break;
-                }
-            }
-            if(!flag)
-                Console.WriteLine("Product Not Found");
-            break;
-          case 4:
-            decision = false;
-            break;
+         case 2:
+           foreach (var prod in products.Values) {
+             Console.WriteLine(prod.display());
+           }
+           break;
 
-          default:
-            Console.WriteLine("Invalid choice. Please try again.");
-            break;
-        }
-      } while (decision);
-    }
-  }
+         case 3:
+           Console.WriteLine("Please Enter the ProdID");
+           prodID = Convert.ToInt32(Console.ReadLine());
+           try {
+             Console.WriteLine(products[prodID].display());
+           } catch (KeyNotFoundException) {
+             Console.WriteLine("Product Not Found");
+           }
+           break;
+
+         case 4:
+           Console.WriteLine("Please Enter the ProdID to delete");
+           prodID = Convert.ToInt32(Console.ReadLine());
+           products.Remove(prodID);
+           break;
+
+         case 5:
+           decision = false;
+           break;
+
+         default:
+           Console.WriteLine("Invalid choice. Please try again.");
+           break;
+       }
+     } while (decision);
+   }
+ }
 }
